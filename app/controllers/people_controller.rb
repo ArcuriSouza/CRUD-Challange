51afe10a47c5c1a4eed176054c2385fc5a2350fc
Gabriel Arcuri <arcuri.souza@hotmail.com>
2025-03-21 22:13:19 -0300

    create CRUD flow

---
+++ app/controllers/people_controller.rb
@@ -0,0 +1,56 @@
+class PeopleController < ApplicationController
+  before_action :load_person, only: %i[show edit update destroy]
+
+  def index
+    @people = Person.all
+  end
+
+  def show; end
+
+  def new
+    @person = Person.new
+    @person.build_address
+  end
+
+  def create
+    @person = Person.new(person_params)
+
+    if @person.save
+      redirect_to people_path
+    else
+      render :new
+    end
+  end
+
+  def edit; end
+
+  def update
+    if @person.update(person_params)
+      redirect_to people_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    if @person.destroy
+      redirect_to people_path
+    else
+
+    end
+  end
+
+  private
+
+  def load_person
+    @person = Person.find(params[:id])
+  end
+
+  def person_params
+    params.require(:person).permit(:name, :email, :phone, :birthdate, address_attributes)
+  end
+
+  def address_attributes
+    { address_attributes: %i[id street city state postal_code country] }
+  end
+end
